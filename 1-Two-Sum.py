31bc06ce4c76d076d534415869c26cce00da4eff
MozBlbn <165378477+MozBlbn@users.noreply.github.com>
2024-07-04 13:42:57 +0300

    [1] [Time Beats: 64.98%] [Memory Beats: 28.01%] - LeetPush

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []  # No solution fo
+        dic = {}
+        for i, v in enumerate(nums):
+            if target - v in dic:
+                return [dic[target - v], i]
+            dic[v] = i
+        return []
